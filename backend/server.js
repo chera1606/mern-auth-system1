20d56548f2c57daa33103b217a4e3fd8822aa4a0
chera1606 <cherinetderbie@gmail.com>
2026-03-08 03:00:48 +0300

    chore: setup Express server and register authRoutes

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
 import connectDB from "./config/mongodb.js";
+import authRouter from "./routes/authRoutes.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -16,5 +17,8 @@ app.use(cors({ credentials: true }));
 
 // API Endpoints
 app.get("/", (req, res) => res.send("Hello from backend"));
+
+// FIXED ROUTE
+app.use("/api/auth", authRouter);
 
 app.listen(port, () => console.log(`server started on PORT:${port}`));
